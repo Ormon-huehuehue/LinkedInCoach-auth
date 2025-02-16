1b61cd2361b518285a45f89ebafcab5d04df3dae
Ormon-huehuehue <armnvrma10@gmail.com>
2025-02-17 03:34:25 +0530

    ionno man

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -3,9 +3,13 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+
+  console.log("/api/session called")
   const cookieStore = cookies();
   const supabase = createClient();
   const { data } = await (await supabase).auth.getSession();
+
+  console.log("data", data)
 
   if (!data.session) {
     return new NextResponse(
@@ -13,9 +17,10 @@ export async function GET() {
       {
         status: 401,
         headers: {
-          "Access-Control-Allow-Origin": "*", // Change "*" to your extension ID in production
+          "Access-Control-Allow-Origin": "chrome-extension://ecmkdildiabljkllajajlapjmdidgkam", 
           "Access-Control-Allow-Methods": "GET, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Credentials": "true", // Allow cookies
         },
       }
     );
@@ -26,7 +31,7 @@ export async function GET() {
     {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*", // Change to your extension ID in production
+        "Access-Control-Allow-Origin": "chrome-extension://ecmkdildiabljkllajajlapjmdidgkam", // Change to your extension ID in production
         "Access-Control-Allow-Methods": "GET, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
@@ -39,7 +44,7 @@ export async function OPTIONS() {
   return new NextResponse(null, {
     status: 204,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "chrome-extension://ecmkdildiabljkllajajlapjmdidgkam",
       "Access-Control-Allow-Methods": "GET, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
